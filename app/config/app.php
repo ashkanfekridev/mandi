8dce75929b70a96862da49b6037ab345ff1d39e8
ashkanfekri <ashkanfekridev.com>
2020-09-27 06:17:48 +0330

    add database configorition items in config file

--- app/config/app.php
+++ app/config/app.php
@@ -2,3 +2,9 @@
 
 //view config
 define('VIEW_PATH', __DIR__.'/../Views/');
+
+//database
+define('DB_HOST', '127.0.0.1'); // localhost or 127.0.0.1
+define('DB_USER', 'root'); // root
+define('DB_PASS', ''); // null or '' for local develop
+define('DB_NAME', 'php_cms'); // your db table name
